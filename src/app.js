4cb9a46bbe8bdebd5d9e2178f0ead706b401cff7
hermanjackson <hermanjackson561@gmail.com>
2021-03-28 18:00:00 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let item = document.querySelectorAll("h1")[0];
+
+  let suit = ["heart", "diamond", "spade", "club"];
+  let selectsuit = suit[getRndInteger(0, suit.length - 1)];
+  let numbers = ["A", "Q", "K", "J", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  card.classList.add(selectsuit);
+  item.innerHTML = numbers[getRndInteger(0, numbers.length - 1)];
 };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
